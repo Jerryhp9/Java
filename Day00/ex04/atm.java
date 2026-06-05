4544cac4f7dc902a0e7b50e6069c500dfe4e5227
jerryhp <horjarry@gmail.com>
2026-06-05 17:49:10 +0800

    completed mini atm program

--- Day00/ex04/atm.java
+++ Day00/ex04/atm.java
@@ -6,12 +6,26 @@ public class atm {
 	public static void main(String[]args)
 	{
 		Scanner newobj = new Scanner(System.in);
-		System.out.println("1. Check Balance\n2. Deposit\n3. Withdraw\n");
-		System.out.print("Choice: ");
-		String prompt = newobj.nextLine();
-		if (Objects.equals(prompt, "Check Balance") || Objects.equals(prompt, "Deposit")
-		|| Objects.equals(prompt, "Withdraw"))
-			
+		double	Balance = 1000.00;
+		while (true)
+		{
+			System.out.println("\n----------MYPiggyBank----------\n");
+			System.out.println("Options:\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Cancel\n");
+			System.out.print("Input: ");
+			String prompt = newobj.nextLine();
+			System.out.println();
+			if (Objects.equals(prompt, "Check Balance") || Objects.equals(prompt, "Deposit")
+			|| Objects.equals(prompt, "Withdraw"))
+				Balance = atm_machine.machine(prompt, Balance);
+			else if (Objects.equals(prompt, "Cancel"))
+			{
+				System.out.println("-- Thank you for choosing our service, have a nice day! --");
+				break;
+			}
+			else
+				System.out.println("Please type in the right input\n");
+			System.out.println("\n-------------------------------");
+		}
 		newobj.close();
 	}
 }
