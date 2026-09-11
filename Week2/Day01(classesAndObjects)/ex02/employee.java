98820699f5e78ef5ebc60e2639e5cd77fc552974
Jerry Hor <jhor@u81z05s03.42kl.edu.my>
2026-09-11 16:44:35 +0800

    experimenting with tight and loose coupling design concepts

--- Week2/Day01(classesAndObjects)/ex02/employee.java
+++ Week2/Day01(classesAndObjects)/ex02/employee.java
@@ -1,3 +1,22 @@
 public class employee {
-	
+	private static int employeeCount;
+
+	public employee() {
+		employeeCount++;
+		System.out.println("Employee created");
+		System.out.println("Total employees: " + employeeCount + "\n");
+	}
+
+	public static void main(String args[]) {
+		employee one = new employee();
+		employee two = new employee();
+		employee three = new employee();
+		employee four = new employee();
+		employee five = new employee();
+		employee six = new employee();
+		employee seven = new employee();
+		employee eight = new employee();
+		employee nine = new employee();
+		employee ten = new employee();
+	}
 }
---
+++ Week2/Day01(classesAndObjects)/ex03/PaymentMethod.java
@@ -0,0 +1,22 @@
+//tight coupling
+
+class creditCard {
+	
+} 
+
+class PaymentMethod {
+	void pay(double amount) {
+		// how do you implement credit card payment method ?
+
+	}
+}
+
+class order {
+	private int orderId;
+	private int amount;
+
+	public void checkout() {
+
+	}
+
+}
